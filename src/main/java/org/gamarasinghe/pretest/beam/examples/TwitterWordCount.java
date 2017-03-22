907ed2186cbade840439b8fd1a8c60803277d8d3
Gayashan Amarasinghe <gayashan.amarasinghe@gmail.com>
2017-03-22 22:24:22 +1100

    Added few comments to walkthrough the scenario.

--- src/main/java/org/gamarasinghe/pretest/beam/examples/TwitterWordCount.java
+++ src/main/java/org/gamarasinghe/pretest/beam/examples/TwitterWordCount.java
@@ -35,12 +35,23 @@ public class TwitterWordCount {
     }
 
     public static void main(String[] args) {
+        // Creating the pipeline using my custom configuration options
         TwitterWordCountOptions options = PipelineOptionsFactory.fromArgs(args).withValidation().as
                 (TwitterWordCountOptions.class);
         Pipeline pipeline = Pipeline.create(options);
+
+        // Get the input and output file names from the configuration options
         String inputFile = options.getInputFile();
         String outputFile = options.getOutputFile();
 
+        // Constructing the pipeline workflow
+        // 1. Read the tweet stream from the csv file
+        // 2. Extract the timestamp from the csv file and attach the timestamp to each event
+        // 3. Extract the tweets from the csv file and put it in the event
+        // 4. Tokenize tweets in to words
+        // 5. Apply windowing to the stream and add an event time based trigger
+        // 6. Count the words based on each window (results in a key-value pair of word-count)
+        // 7. Format the output per each word-count to be printed/displayed.
         PCollection<String> twitterStream = pipeline.apply("ReadTweetsAndTimestamps", TextIO.Read.from(inputFile))
                 .apply("Set event time and extract tweets", ParDo.of(new SetTimestampAndExtractTextFn()))
                 .apply("Tokenize Tweets Into Words", ParDo.of(new ExtractWords()))
@@ -49,13 +60,16 @@ public class TwitterWordCount {
                                 .triggering(AfterWatermark.pastEndOfWindow()
                                         .withEarlyFirings(AfterProcessingTime.pastFirstElementInPane()
                                                 .plusDelayOf(Duration.standardMinutes(3)))
-                                        .withLateFirings(AfterPane.elementCountAtLeast(5)))
+                                        .withLateFirings(AfterPane.elementCountAtLeast(1)))
                                 .withAllowedLateness(Duration.standardMinutes(4))
                                 .accumulatingFiredPanes())
                 .apply("Get Word count", Count.<String>perElement())
                 .apply("Format the KV pairs", MapElements.via(new SimpleKeyValueFormatter()));
 
+        // 8. Write the results to file(s)
         twitterStream.apply("Write results to text file(s)", TextIO.Write.to(outputFile));
+
+        // 9. Execute the pipeline that you have constructed
         pipeline.run();
     }
 
