d2755bf64affe93cb5df97e8df9f7f2d54878c76
Gayashan Amarasinghe <gayashan.amarasinghe@gmail.com>
2017-03-22 23:05:20 +1100

    Added GroupByKey transform to the pipeline.

--- src/main/java/org/gamarasinghe/pretest/beam/examples/TwitterWordCount.java
+++ src/main/java/org/gamarasinghe/pretest/beam/examples/TwitterWordCount.java
@@ -24,13 +24,15 @@ public class TwitterWordCount {
      */
     public interface TwitterWordCountOptions extends PipelineOptions {
         @Description("Path of the file to read from")
-        @Default.String("/home/gayashan/projects/research/twitterwordcountbeam/src/main/resources/dataset2_small.csv")
+        @Default.String("/home/gayashan/projects/research/twitterwordcountbeam/src/main/resources/dataset2_medium.csv")
         String getInputFile();
+
         void setInputFile(String value);
 
         @Description("Path of the file to write to")
         @Default.String("results/twitter-wordcount-output")
         String getOutputFile();
+
         void setOutputFile(String value);
     }
 
@@ -52,7 +54,8 @@ public class TwitterWordCount {
         // 5. Apply windowing to the stream and add an event time based trigger
         // 6. Count the words based on each window (results in a key-value pair of word-count)
         // 7. Format the output per each word-count to be printed/displayed.
-        PCollection<String> twitterStream = pipeline.apply("ReadTweetsAndTimestamps", TextIO.Read.from(inputFile))
+        PCollection<String> twitterStream = pipeline
+                .apply("ReadTweetsAndTimestamps", TextIO.Read.from(inputFile))
                 .apply("Set event time and extract tweets", ParDo.of(new SetTimestampAndExtractTextFn()))
                 .apply("Tokenize Tweets Into Words", ParDo.of(new ExtractWords()))
                 .apply("Apply Windowing And Triggers",
@@ -64,7 +67,9 @@ public class TwitterWordCount {
                                 .withAllowedLateness(Duration.standardMinutes(4))
                                 .accumulatingFiredPanes())
                 .apply("Get Word count", Count.<String>perElement())
-                .apply("Format the KV pairs", MapElements.via(new SimpleKeyValueFormatter()));
+                .apply("Create Window to wordcount mapping", ParDo.of(new SetWordcountWindowMap()))
+                .apply("Groupby key in each window", GroupByKey.<IntervalWindow, KV<String, Long>>create())
+                .apply("Format wordcount per window to String", ParDo.of(new FormatWordcountPerWindow()));
 
         // 8. Write the results to file(s)
         twitterStream.apply("Write results to text file(s)", TextIO.Write.to(outputFile));
@@ -113,6 +118,24 @@ public class TwitterWordCount {
             String result = input.getKey() + ":" + input.getValue();
             System.out.println(result);
             return result;
+        }
+    }
+
+    private static class FormatWordcountPerWindow extends DoFn<KV<IntervalWindow, Iterable<KV<String, Long>>>, String> {
+        @ProcessElement
+        public void processElement(ProcessContext context) {
+            System.out.println(context.element());
+            for (KV<String, Long> wordcount : context.element().getValue()) {
+                String wordcountOutput = wordcount.getKey() + ":" + wordcount.getValue();
+                context.output(wordcountOutput);
+            }
+        }
+    }
+
+    private static class SetWordcountWindowMap extends DoFn<KV<String, Long>, KV<IntervalWindow, KV<String, Long>>> {
+        @ProcessElement
+        public void processElement(ProcessContext context, IntervalWindow window) {
+            context.output(KV.of(window, context.element()));
         }
     }
 }
