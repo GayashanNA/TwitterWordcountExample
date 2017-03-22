977ca2371814ac52437689c35d397e752644400c
Gayashan Amarasinghe <gayashan.amarasinghe@gmail.com>
2017-03-22 14:49:46 +1100

    Added pipeline configuration options and modified the trigger.

--- src/main/java/org/gamarasinghe/pretest/beam/examples/TwitterWordCount.java
+++ src/main/java/org/gamarasinghe/pretest/beam/examples/TwitterWordCount.java
@@ -2,8 +2,7 @@ package org.gamarasinghe.pretest.beam.examples;
 
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
-import org.apache.beam.sdk.options.PipelineOptions;
-import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.options.*;
 import org.apache.beam.sdk.transforms.*;
 import org.apache.beam.sdk.transforms.windowing.*;
 import org.apache.beam.sdk.values.KV;
@@ -19,21 +18,39 @@ import java.util.List;
  */
 public class TwitterWordCount {
 
+    /***
+     * Custom pipeline configuration options to get the input file name and the output file name.
+     * In case these values are not given via the command line arguments, the default values will be used.
+     */
+    public interface TwitterWordCountOptions extends PipelineOptions {
+        @Description("Path of the file to read from")
+        @Default.String("/home/gayashan/projects/research/twitterwordcountbeam/src/main/resources/dataset2_small.csv")
+        String getInputFile();
+        void setInputFile(String value);
+
+        @Description("Path of the file to write to")
+        @Default.String("results/twitter-wordcount-output")
+        String getOutputFile();
+        void setOutputFile(String value);
+    }
 
     public static void main(String[] args) {
-        PipelineOptions options = PipelineOptionsFactory.fromArgs(args).create();
+        TwitterWordCountOptions options = PipelineOptionsFactory.fromArgs(args).withValidation().as
+                (TwitterWordCountOptions.class);
         Pipeline pipeline = Pipeline.create(options);
-        String inputFile =
-                "/home/gayashan/projects/research/twitterwordcountbeam/src/main/resources/dataset2_medium.csv";
-        String outputFile = "results/output.csv";
+        String inputFile = options.getInputFile();
+        String outputFile = options.getOutputFile();
 
         PCollection<String> twitterStream = pipeline.apply("ReadTweetsAndTimestamps", TextIO.Read.from(inputFile))
                 .apply("Set event time and extract tweets", ParDo.of(new SetTimestampAndExtractTextFn()))
                 .apply("Tokenize Tweets Into Words", ParDo.of(new ExtractWords()))
                 .apply("Apply Windowing And Triggers",
-                        Window.<String>into(FixedWindows.of(Duration.standardMinutes(5)))
-                                .triggering(AfterWatermark.pastEndOfWindow())
-                                .withAllowedLateness(Duration.standardMinutes(2))
+                        Window.<String>into(FixedWindows.of(Duration.standardMinutes(10)))
+                                .triggering(AfterWatermark.pastEndOfWindow()
+                                        .withEarlyFirings(AfterProcessingTime.pastFirstElementInPane()
+                                                .plusDelayOf(Duration.standardMinutes(3)))
+                                        .withLateFirings(AfterPane.elementCountAtLeast(5)))
+                                .withAllowedLateness(Duration.standardMinutes(4))
                                 .accumulatingFiredPanes())
                 .apply("Get Word count", Count.<String>perElement())
                 .apply("Format the KV pairs", MapElements.via(new SimpleKeyValueFormatter()));
@@ -52,7 +69,7 @@ public class TwitterWordCount {
             if (length > 2) {
                 String tapp = attributes.get(1);
                 // get the tweets from the data elements
-                // in case the tweets contain commas, join them
+                // in case the tweets contain commas, they will be separated by the previous split. So join them.
                 String tweet = StringUtils.join(attributes.subList(3, length), ",");
                 if (Character.isDigit(tapp.charAt(0))) {
                     Instant timestamp = new Instant(Long.parseLong(tapp));
@@ -66,14 +83,6 @@ public class TwitterWordCount {
         @ProcessElement
         public void processElement(ProcessContext context) {
             for (String word : context.element().split("[^a-zA-Z']+")) {
-                String urlRegex = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
-                if (word.toLowerCase().matches(urlRegex) || word.isEmpty() || !Character.isAlphabetic(word.charAt(0))) {
-                    if (!word.isEmpty() && (word.charAt(0) == '#' || word.charAt(0) == '@')) {
-                        context.output(word.trim());
-                    } else {
-                        continue;
-                    }
-                }
                 if (!word.isEmpty()) {
                     context.output(word.trim());
                 }
